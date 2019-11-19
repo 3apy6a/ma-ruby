a9a28e3af09d330e8645dfa2c81065bde028b089
3apy6a <zaruba.av@gmail.com>
2019-11-19 23:07:18 +0200

    correcting remarks of task 4

--- Ruby Basic/task4.rb
+++ Ruby Basic/task4.rb
@@ -1,21 +1,21 @@
-p 'input boolean true or false'
+puts 'input boolean true or false:'
 
-input_value = gets.chomp
+input_value = gets.chomp.downcase
 
-if input_value == 'true'
-  input_value = true
-elsif input_value == 'false'
-  input_value = false
-end
+boolean_keywords = ["true", "false"]
+
+abort('You need enter true/false keyword.') unless boolean_keywords.include? input_value
+
+input_value.include?("true") ? input_value = true : input_value = false
 
 puts "input class is: #{input_value.class}"
 
 input_value_int = input_value ? 1 : 0
 
-p "convert boolean: #{p input_value} to integer: #{input_value_int}"
+puts "convert boolean: #{p input_value} to integer: #{input_value_int}"
 
-p "Size of integer type  #{input_value_int.size.to_s} bite"
+puts "Size of integer type  #{input_value_int.size} bite"
 
 input_value = input_value.to_s
 
-p "Size of string tipe: #{input_value.bytesize.to_s} bite"
+puts "Size of string tipe: #{input_value.bytesize} bite"
