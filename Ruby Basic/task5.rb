def5227aabeb158e49b07960b758106d71780274
3apy6a <zaruba.av@gmail.com>
2019-11-20 17:11:00 +0200

    refactoring the task5

--- Ruby Basic/task5.rb
+++ Ruby Basic/task5.rb
@@ -5,10 +5,10 @@ puts "Going to open '#{filename}'"
 text = open filename
 begin
   text.each.with_index(1) do |line, number_of_string|
-    if line.include?(find_string)
-      p "Number of the required line: #{number_of_string}"
-      break
-    end
+    next unless line.include?(find_string)
+
+    p "Number of the required line: #{number_of_string}"
+    break
   end
 ensure 
   text&.close
