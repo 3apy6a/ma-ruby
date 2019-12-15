cf13d7f0edd3641dbf434551dc9a1cfff2920b0f
3apy6a <zaruba.av@gmail.com>
2019-12-15 16:54:53 +0200

    deleted renamed files

--- Active_Record/app/models/booking_gears.rb
+++
@@ -1,5 +0,0 @@
-class BookingGear < ActiveRecord::Base
-  belongs_to :booking
-  belongs_to :gear
-
-end
--- Active_Record/config/initializers/initializers.rb
+++
@@ -1,7 +0,0 @@
-require './config/initializers/database'
-require './app/models/customer'
-require './app/models/sup'
-require './app/models/booking'
-require './app/models/booking_gears'
-require './app/models/gear'
-
