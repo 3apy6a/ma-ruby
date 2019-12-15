ed170195604b516900b782fbb24c504b0481bfd6
3apy6a <zaruba.av@gmail.com>
2019-12-15 21:36:22 +0200

    files for new db db_sup

--- Active_Record/run.rb
+++ Active_Record/run.rb
@@ -1,18 +1,14 @@
-require './config/initializers/initializers'
-
-
+# frozen_string_literal: true
+require './config/initializers/initializer'
 sup_1 = Sup.new(
-    model: "Gladiator 10,6",
-    avatar_url: "https://sup_photo.com",
+        model: 'Red Paddle Voyager 13,2"',
+    length: '13,2"',
+    preview_url: 'https://www.inflatableboarder.com/wp-content/uploads/2019/08/red-paddle-co-13-2-voyager.jpg',
     user_quantity: 1,
-    desc: "Oneuser SUP lenth -10,6"
-)
+    description: 'Oneuser SUP, lenth -13,2 for touring'
+  )
+
+
 puts "Sup: #{sup_1.id}"
-
-if sup_1.save
-  puts 'Saved'
-else
-  puts "something went wrong"
-end
-
-puts"Sup: #{sup_1.full_model}"
+puts sup_1.save ? 'Saved' : 'something went wrong'
+puts "Sup: #{sup_1.full_model}"
