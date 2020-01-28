f7c027b58125ae649cd932d242334143f30400b9
3apy6a <zaruba.av@gmail.com>
2020-01-29 00:21:40 +0200

    refactor

--- Rack/app/models/booking.rb
+++ Rack/app/models/booking.rb
@@ -15,6 +15,8 @@ class Booking < ActiveRecord::Base
   private
 
   def end_date_after_start_date
-    errors.add(:end_date, 'must be after the start date') unless end_date < start_date
+    if end_date < start_date
+      errors.add(:end_date, 'must be after the start date')
+    end
   end
 end
